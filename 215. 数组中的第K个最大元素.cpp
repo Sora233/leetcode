ececffcd2bccf8d9bf96824cf978ad8fb44d3c0a
sora233 <sora@sora233.me>
2021-04-10 20:56:11 +0800

    215

---
+++ 215. 数组中的第K个最大元素.cpp
@@ -0,0 +1,11 @@
+
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        nth_element(nums.begin(), nums.begin()+k-1, nums.end(), greater<int>());
+        return nums[k-1];
+    }
+};
